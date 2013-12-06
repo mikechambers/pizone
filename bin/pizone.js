6590fa96b6d6f04f3def13ca9c57cceff34a8fe0
Mike Chambers <mikechambers@gmail.com>
2013-12-06 14:43:45 -0800

    fixed incorrect mac address format. continued porting to node.

--- bin/pizone.js
+++ bin/pizone.js
@@ -1,44 +1,86 @@
 #!/usr/local/bin/node
 
+var FileSystem = require('fs');
 //nohup node simple-server.js > output.log &
 
 
 //file path to file containing JSON data of mac addresses
-var ADDRESSES_JSON = "/etc/pizone/address.json";
+var ADDRESS_DATA_PATH = "/etc/pizone/address.json";
 
 //interval in milliseconds that address will be rotated
 var REFRESH_INTERVAL = 100 * 60 * 1; //1 minute
 
+var addresses = [];
+var intervalId;
+var index = 0;
 
 var ROOT_ID = 0;
 //chech that we are running as root
 if(process.getuid() != ROOT_ID){
 	console.log("Script must be run as root.");
 	process.exit(1);
-}
-
-
+};
 
 var init = function() {
-	//load json data and store in varriable
-	//start main 
-}
+	loadAddressData(
+		function(){
+			resetInterval();
+		},
+		function (err) {
+			//what should we do here?
+			//right now we error and exit
+			console.log("Error : main : cannot load adddress data. Aborting.");
+			process.exit(1);
+		});
+};
 
-var main = function() {
-	//start http server
-	//start runtime loop
-}
+var loadAddressData = function(onSuccessCallback, onErrorCallback) {
+	
+	FileSystem.readFile(ADDRESS_DATA_PATH, 'utf8', function (err, data) {
+		
+		if (err) {
+			console.log("loadAddressData");
+			console.log(err);
+			onErrorCallback(err);
+			return;
+		}
+
+		//should e explicitly check its length?
+		if(!data) {
+			//error
+		}
+		
+		addresses = JSON.parse(data);
+		onSuccessCallback();
+	});
+};
+
+var resetInterval = function(){
+	if(intervalId) {
+		clearInterval(intervalId);
+		intervalId = 0;
+	}
+	
+	intervalId = setInterval(
+		onInterval,
+		REFRESH_INTERVAL);
+};
 
 var onInterval = function() {
 	
-}
+	if(!addresses.length) {
+		index = 0;
+		return;
+	}
+	
+	var item = addresses[index];
+	console.log(item);
+	
+	index++;
+	
+	if (index >= addresses.length) {
+		index = 0;
+	}	
+};
 
 init();
-
-
-var index = 0;
-
-var timerId = setInterval(
-	function (){
-		console.log(index++);
-	}, REFRESH_INTERVAL)
