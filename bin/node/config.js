26192d198dc5690ee66844c19561beb4f03dad13
Mike Chambers <mikechambers@gmail.com>
2013-12-08 16:10:10 -0800

    renaming

--- bin/node/config.js
+++ bin/node/config.js
@@ -11,7 +11,7 @@
     }
     
     //file path to file containing JSON data of mac addresses
-    exports.ADDRESS_DATA_PATH = "/etc/pizone/address.json";
+    exports.ADDRESS_DATA_PATH = "/etc/pizone/addresses.json";
     
     //interval in milliseconds that address will be rotated
     exports.REFRESH_INTERVAL = 1000 * 60 * 10;//10 minutes
