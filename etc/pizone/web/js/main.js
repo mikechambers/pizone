67f8b4ebb966bf6144a816a2261e8eccc5cd1301
Mike Chambers <mikechambers@gmail.com>
2013-12-27 12:32:11 -0800

    time intervals now formatted correctly.

--- etc/pizone/web/js/main.js
+++ etc/pizone/web/js/main.js
@@ -4,19 +4,47 @@
 (function () {
     "use strict";
     
-    var main = function () {
-        
-        var Handlebars = window.Handlebars;
-        
-        Handlebars.registerHelper("getStatus", function(address, currentAddress ) {
-            if(address == currentAddress)
-            {
-                return "active";
-            } else {
-                return "";
+    var Handlebars = window.Handlebars;
+    
+    Handlebars.registerHelper("getStatus", function (address, currentAddress) {
+        if (address === currentAddress) {
+            return "active";
+        } else {
+            return "";
+        }
+    });
+    
+    var formatTimeInterval = function (prop, propName) {
+        var out = "";
+        if (prop) {
+            out += prop + " " + propName;
+            
+            if (prop !== 1) {
+                out += "s";
             }
-        });        
+            out += " ";
+        }
         
+        return out;
+    };
+    
+    var formatTime = function (ms) {
+
+        //var milliseconds = parseInt((duration%1000)/100)
+        var seconds = parseInt((ms / 1000) % 60, 10);
+        var minutes = parseInt((ms / (1000 * 60)) % 60, 10);
+        var hours = parseInt((ms / (1000 * 60 * 60)) % 24, 10);
+
+        var out = "";
+        out += formatTimeInterval(hours, "hour");
+        out += formatTimeInterval(minutes, "minute");
+        out += formatTimeInterval(seconds, "second");
+        
+        return out;
+    };
+    
+    var main = function () {
+                
         $.ajax({
             url: "/api/getinfo",
             cache: false,
@@ -34,15 +62,20 @@
             }
             $("#currentCPUTemp").text(temp);
             
-            if(currentItem.timeSinceUpdate) {
-                $("#timeToNextAP").text((data.config.refreshInterval - currentItem.timeSinceUpdate) + " ms");
+
+            $("#updateInterval").text("Every " + formatTime(data.config.refreshInterval));
+            
+            if (currentItem.timeSinceUpdate) {
+                var _t = (data.config.refreshInterval - currentItem.timeSinceUpdate);
+
+                $("#timeToNextAP").text(formatTime(_t));
             }
             
             var source = $("#ap-list-template").html();
-            var apListTemplate = Handlebars.compile(source); 
-            var html = apListTemplate({"addresses":data.addresses, "currentAddress": currentItem.address});
+            var apListTemplate = Handlebars.compile(source);
+            var html = apListTemplate({"addresses": data.addresses, "currentAddress": currentItem.address});
             
-             $("#ap-list-body").append(html);
+            $("#ap-list-body").append(html);
         });
     };
     
